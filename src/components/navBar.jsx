0697475b15e2e2b93fa001d3599455731cb30132
Hamza BAROUDI <61873638+baroudu1@users.noreply.github.com>
2022-05-22 07:15:36 +0100

    bugs

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -10,28 +10,24 @@ function NavBar() {
 
         <Navbar bg="light" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                <Navbar.Brand href="#">Home</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
+                <Navbar.Collapse id="navbarScroll" >
                     <Nav
-                        className="me-auto my-2 my-lg-0"
+                        className="ml-auto my-2 my-lg-0"
                         style={{ maxHeight: "100px" }}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">
-                                Another action
-                            </NavDropdown.Item>
+
+                        <NavDropdown title="Sign In / Sign Up" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#/login">Sign In</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">
-                                Something else here
-                            </NavDropdown.Item>
+
+                            <NavDropdown.Item href="#/register">Sign Up</NavDropdown.Item>
+                            
                         </NavDropdown>
                     </Nav>
-                    
+
                 </Navbar.Collapse>
             </Container>
         </Navbar>
