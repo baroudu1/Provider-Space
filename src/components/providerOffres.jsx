3377078342c1be452515ec6b2bf496317c024e0a
Hamza BAROUDI <61873638+baroudu1@users.noreply.github.com>
2022-05-22 07:02:30 +0100

    micro provider

--- src/components/providerOffres.jsx
+++ src/components/providerOffres.jsx
@@ -6,74 +6,8 @@ import { getAllOffres } from "../services/providerOffresService"
 import { Offer } from "./offer";
 
 function ProviderOffres() {
-    let oo = [
-        {
-            "id": "12445241",
-            "title": "12445244",
-            "description": "12445244",
-            "resources": [
-                {
-                    "cpu": "1551",
-                    "ram": "1551",
-                    "screen": "1551",
-                    "resolution": "1551",
-                    "qte": "1551"
-                }
-            ],
-            "date": "12445244"
-
-        },
-        {
-            "id": "1244524",
-            "title": "12445244",
-            "description": "12445244",
-            "resources": [
-                {
-                    "cpu": "1551",
-                    "ram": "1551",
-                    "screen": "1551",
-                    "resolution": "1551",
-                    "qte": "1551"
-                }
-            ],
-            "date": "12445244"
-
-        },
-        {
-            "id": "124452",
-            "title": "12445244",
-            "description": "12445244",
-            "resources": [
-                {
-                    "cpu": "1551",
-                    "ram": "1551",
-                    "screen": "1551",
-                    "resolution": "1551",
-                    "qte": "1551"
-                }
-            ],
-            "date": "12445244"
-
-        },
-        {
-            "id": "12444",
-            "title": "12445244",
-            "description": "12445244",
-            "resources": [
-                {
-                    "cpu": "1551",
-                    "ram": "1551",
-                    "screen": "1551",
-                    "resolution": "1551",
-                    "qte": "1551"
-                }
-            ],
-            "date": "12445244"
-
-        }
-    ]
+    
     let [offres, setOffres] = useState([]);
-    let [offre, setOffre] = useState();
     let [count, setCount] = useState();
 
 
